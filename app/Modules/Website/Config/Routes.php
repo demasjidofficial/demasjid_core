ea1029f8e5cbfa9cf25fdf984f94d7113217473b
ahmad <ahmad.afandi85@gmail.com>
2022-03-08 15:36:56 +0700

    update

--- app/Modules/Website/Config/Routes.php
+++ app/Modules/Website/Config/Routes.php
@@ -3,10 +3,9 @@
 namespace App\Modules\Config;
 
 $routes->group(ADMIN_AREA, ['namespace' => '\App\Modules\Website\Controllers'], static function ($routes) {
-    $routes->resource('website/menus', ['controller' => 'SitemenusController']);
-    /*
-    $routes->resource('website/jabatan', ['controller' => 'JabatanController']);
-    $routes->resource('website/wilayah', ['controller' => 'WilayahController']);
-    $routes->resource('website/member', ['controller' => 'MemberController']);
-    */
+    $routes->resource('website/sitemenus', ['controller' => 'SitemenusController']);
+    $routes->resource('website/sitepages', ['controller' => 'SitepagesController']);
+    $routes->resource('website/siteposts', ['controller' => 'SitepostsController']);
+    $routes->resource('website/sitesections', ['controller' => 'SitesectionsController']);
+    $routes->resource('website/sitesocials', ['controller' => 'SitesocialsController']);    
 });
