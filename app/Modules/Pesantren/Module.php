d30dfdc16c923b3ae01607e7654303c4dde0d81d
ahmad <ahmad.afandi85@gmail.com>
2022-03-09 20:01:31 +0700

    misc: persiapan fix admin bonfire

---
+++ app/Modules/Pesantren/Module.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Modules\Pesantren;
+
+use App\Config\BaseModule;
+use Bonfire\Libraries\Menus\MenuItem;
+
+/**
+ * Pengurus Module setup.
+ */
+class Module extends BaseModule
+{
+    /**
+     * Setup our admin area needs.
+     */
+    public function initAdmin()
+    {
+        helper('url');
+        // Settings menu for sidebar
+        $sidebar = service('menus');
+
+        // Content Menu for sidebar
+        $pengurusItem = new MenuItem([
+            'title'           => 'Pengurus',
+            'url'             => url_to('App\Modules\Masjid\Controllers\PengurusController::index'),
+            'fontAwesomeIcon' => 'fas fa-users',
+            'permission'      => 'masjid.pengurus.list',
+        ]);
+        $jabatanItem = new MenuItem([
+            'title'           => 'Jabatan',
+            'url'             => url_to('App\Modules\Masjid\Controllers\JabatanController::index'),
+            'fontAwesomeIcon' => 'fas fa-users',
+            'permission'      => 'masjid.jabatan.list',
+        ]);
+        $memberItem = new MenuItem([
+            'title'           => 'Member',
+            'url'             => url_to('App\Modules\Masjid\Controllers\MemberController::index'),
+            'fontAwesomeIcon' => 'fas fa-users',
+            'permission'      => 'masjid.member.list',
+        ]);
+        $wilayahItem = new MenuItem([
+            'title'           => 'Wilayah',
+            'url'             => url_to('App\Modules\Masjid\Controllers\WilayahController::index'),
+            'fontAwesomeIcon' => 'fas fa-maps',
+            'permission'      => 'masjid.wilayah.list',
+        ]);
+        // $sidebar->menu('sidebar')->collection('pesantren')->addItem($jabatanItem)->addItem($pengurusItem)->addItem($memberItem)->addItem($wilayahItem);
+    }
+}
