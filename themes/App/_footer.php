867614ae77559e69a8fac0540b0c4bc9e012cd7c
KRALTEK <dev.kreasialamteknologi@gmail.com>
2022-04-21 05:07:42 +0530

    adjust footer header

---
+++ themes/App/_footer.php
@@ -0,0 +1,103 @@
+<footer>
+        <div class="footer-wrapper section-bg2" data-background="assets/app/theme-charityworks/img/gallery/footer_bg.png">
+            <!-- Footer Top-->
+            <div class="footer-area footer-padding" id="kontak">
+                <div class="container">
+                    <div class="row d-flex justify-content-between">
+                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
+                        <div class="single-footer-caption mb-50">
+                            <div class="single-footer-caption mb-30">
+                                <div class="footer-tittle">
+                                    <div class="footer-logo mb-20">
+                                        <a href="#"><img src="/assets/app/theme-charityworks/img/logo/logo2_footer.png" alt=""></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
+                            <div class="single-footer-caption mb-50">
+                                <div class="footer-tittle">
+                                    <h4><?= lang('app.contact_info')?></h4>
+                                    <ul>
+                                        <li>
+                                            <p><?= lang('app.alamat')?> :<br/><?= $masjid_profile['address'];?> <br/>[city_name] [postal_code]</p>
+                                        </li>
+                                        <li><a href="#"><?= lang('app.phone')?> : <br/><?= $masjid_profile['telephone'];?></a></li>
+                                        <li><a href="#"><?= lang('app.email')?> : <br/><?= $masjid_profile['email'];?></a></li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
+                            <div class="single-footer-caption mb-50">
+                                <div class="footer-tittle">
+                                    <h4><?= lang('app.important_link')?></h4>
+                                    <ul>
+                                        <li><a href="#"><?= lang('app.article')?></a></li>
+                                        <li><a href="#"><?= lang('app.agenda')?></a></li>
+                                        <li><a href="#">Camp</a></li>
+                                        <li><a href="#">Care</a></li>
+                                        <li><a href="#">FAQ</a></li>
+                                        <li><a href="#">Kebijakan Privasi</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
+                            <div class="single-footer-caption mb-50">
+                                <div class="footer-tittle">
+                                    <h4><?= lang('app.buletin')?> DeMasjid</h4>
+                                    <div class="footer-pera footer-pera2">
+                                    <p><?= lang('app.buletin_desc')?></p>
+                                </div>
+                                <!-- Form -->
+                                <div class="footer-form" >
+                                    <div id="mc_embed_signup">
+                                        <form target="_blank" action=""
+                                        method="get" class="subscribe_form relative mail_part">
+                                            <input type="email" name="email" id="newsletter-form-email" placeholder="<?= lang('app.email_address')?>"
+                                            class="placeholder hide-on-focus" onfocus="this.placeholder = ''"
+                                            onblur="this.placeholder = ' Email Address '">
+                                            <div class="form-icon">
+                                                <button type="submit" name="submit" id="newsletter-submit"
+                                                class="email_icon newsletter-submit button-contactForm"><img src="/assets/app/theme-charityworks/img/gallery/form.png" alt=""></button>
+                                            </div>
+                                            <div class="mt-10 info"></div>
+                                        </form>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- footer-bottom -->
+            <div class="footer-bottom-area">
+                <div class="container">
+                    <div class="footer-border">
+                        <div class="row d-flex justify-content-between align-items-center">
+                            <div class="col-xl-8 col-lg-8 ">
+                                <div class="footer-copy-right">
+                                    <p>
+                                    <?= lang('app.copyright')?> &copy;<script>document.write(new Date().getFullYear());</script>. <?= lang('app.developedby')?> <a href="<?= prep_url('https://demasjid.com') ?>" target="_blank">Demasjid Team</a>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3">
+                                <div class="footer-social f-right">
+                                    <?php foreach($masjid_socials as $item) : ?>
+                                        <a href="<?= $item['link']?>" target="_blank">
+                                          <i class="<?= $item['path_icon']?>"></i>
+                                        </a>
+                                    <?php endforeach ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
--- themes/App/master.php
+++ themes/App/master.php
@@ -27,7 +27,7 @@
 </head>
 <body>
 
-    <!-- ? Preloader Start -->
+    <!-- Preloader Start -->
     <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="preloader-inner position-relative">
@@ -40,131 +40,8 @@
     </div>
     <!-- Preloader Start -->
 
-    <header>
-        <!-- Header Start -->
-        <div class="header-area">
-            <div class="main-header ">
-                <div class="header-top d-none d-lg-block">
-                    <div class="container-fluid">
-                        <div class="col-xl-12">
-                            <div class="row d-flex justify-content-between align-items-center">
-                                <div class="header-info-left d-flex">
-                                    <ul>     
-                                        <li><?= lang('app.phone')?>: +62 851 6136 4811</li>
-                                        <li><?= lang('app.email')?>: masjidalfurqonsby@demasjid.com</li>
-                                    </ul>
-                                    <div class="header-social">    
-                                        <ul>
-                                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="header-info-right d-flex align-items-center">
-                                    <div class="select-this">
-                                        <form action="#">
-                                            <div class="select-itms">
-                                                <select name="select" id="select-lang">
-                                                    <option value="/id"><?= lang('app.indonesia')?></option>
-                                                    <option value="/sa"><?= lang('app.arab')?></option>
-                                                    <option value="/en"><?= lang('app.english')?></option>
-                                                </select>
-                                            </div>
-                                        </form>
-                                    </div>
-                                    <ul class="contact-now">    
-                                    <li>
-                                        <?php if (auth()->loggedIn()) : ?>
-                                        <a href="/logout" ><?= strtoupper(lang('app.logout'))?></a>
-                                        <?php else : ?>
-                                        <a href="/login"><?= strtoupper(lang('app.login'))?></a>
-                                        <?php endif ?>
-                                    </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="header-bottom  header-sticky">
-                    <div class="container-fluid">
-                        <div class="row align-items-center">
-                            <!-- Logo -->
-                            <div class="col-xl-2 col-lg-2">
-                                <div class="logo">
-                                    <a href="#"><img src="/assets/app/theme-charityworks/img/logo/logo.png" alt=""></a>
-                                </div>
-                            </div>
-                            <div class="col-xl-10 col-lg-10">
-                                <div class="menu-wrapper  d-flex align-items-center justify-content-end">
-                                    <!-- Main-menu -->
-                                    <div class="main-menu d-none d-lg-block">
-                                        <nav>
-                                            <ul id="navigation">                                                                                          
-                                                <li><a href="/"><?= lang('app.home')?></a></li>
-                                                <li><a href="#"><?= lang('app.about')?></a>
-                                                    <ul class="submenu">
-                                                        <li><a href="#"><?= lang('app.vision_mission')?></a></li>
-                                                        <li><a href="#"><?= lang('app.structure')?></a></li>
-                                                        <li><a href="#"><?= lang('app.commitee')?></a></li>
-                                                        <li><a href="#"><?= lang('app.erector')?></a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#"><?= lang('app.services')?></a>
-                                                    <ul class="submenu">
-                                                        <li><a href="#"><?= lang('app.zakat')?></a></li>
-                                                        <li><a href="#"><?= lang('app.infaqshodaqoh')?></a></li>
-                                                        <li><a href="#"><?= lang('app.wakaf')?></a></li>
-                                                        <li><a href="#"><?= lang('app.qurban')?></a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#"><?= lang('app.program')?></a>
-                                                    <ul class="submenu">
-                                                        <li><a href="#"><?= lang('app.kajian')?></a></li>
-                                                        <li><a href="#"><?= lang('app.pesantren')?></a></li>
-                                                        <li><a href="#"><?= lang('app.tpq')?></a></li>
-                                                        <!--
-                                                        <li><a href="#">< ?= lang('app.construction')?></a></li>
-                                                        -->
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#"><?= lang('app.muamalah')?></a>
-                                                    <ul class="submenu">
-                                                        <li><a href="#"><?= lang('app.room')?></a></li>
-                                                        <li><a href="#"><?= lang('app.net')?></a></li>
-                                                        <li><a href="#"><?= lang('app.share')?></a></li>
-                                                        <li><a href="#"><?= lang('app.life')?></a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#"><?= lang('app.reports')?></a>
-                                                    <ul class="submenu">
-                                                        <li><a href="#"><?= lang('app.finance_reports')?></a></li>
-                                                        <li><a href="#"><?= lang('app.construction_reports')?></a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#kontak"><?= lang('app.contact')?></a></li>
-                                            </ul>
-                                        </nav>
-                                    </div>
-                                    <!-- Header-btn -->
-                                    <div class="header-right-btn d-none d-lg-block ml-20">
-                                        <a href="#" class="btn header-btn"><?= lang('app.donation')?></a>
-                                    </div>
-                                </div>
-                            </div> 
-                            <!-- Mobile Menu -->
-                            <div class="col-12">
-                                <div class="mobile_menu d-block d-lg-none"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Header End -->
-    </header>
+    <!-- header start -->
+    <?= $this->include('_header') ?>
     <!-- header end -->
 
     <!--aside id="alerts-wrapper">
@@ -175,108 +52,9 @@
         <?= $this->renderSection('main') ?>
     </main>
 
-    <footer>
-        <div class="footer-wrapper section-bg2" data-background="assets/app/theme-charityworks/img/gallery/footer_bg.png">
-            <!-- Footer Top-->
-            <div class="footer-area footer-padding" id="kontak">
-                <div class="container">
-                    <div class="row d-flex justify-content-between">
-                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
-                        <div class="single-footer-caption mb-50">
-                            <div class="single-footer-caption mb-30">
-                                <div class="footer-tittle">
-                                    <div class="footer-logo mb-20">
-                                        <a href="#"><img src="/assets/app/theme-charityworks/img/logo/logo2_footer.png" alt=""></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4><?= lang('app.contact_info')?></h4>
-                                    <ul>
-                                        <li>
-                                            <p><?= lang('app.alamat')?> :<br/>Jl. Teuku Imam Bonjol 99 Surabaya 60299</p>
-                                        </li>
-                                        <li><a href="#"><?= lang('app.phone')?> : <br/>+62 851 6136 4811</a></li>
-                                        <li><a href="#"><?= lang('app.email')?> : <br/>masjidalfurqonsby@demasjid.com</a></li>
-                                    </ul>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4><?= lang('app.important_link')?></h4>
-                                    <ul>
-                                        <li><a href="#"><?= lang('app.article')?></a></li>
-                                        <li><a href="#"><?= lang('app.agenda')?></a></li>
-                                        <li><a href="#">Camp</a></li>
-                                        <li><a href="#">Care</a></li>
-                                        <li><a href="#">FAQ</a></li>
-                                        <li><a href="#">Kebijakan Privasi</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4><?= lang('app.buletin')?> DeMasjid</h4>
-                                    <div class="footer-pera footer-pera2">
-                                    <p><?= lang('app.buletin_desc')?></p>
-                                </div>
-                                <!-- Form -->
-                                <div class="footer-form" >
-                                    <div id="mc_embed_signup">
-                                        <form target="_blank" action=""
-                                        method="get" class="subscribe_form relative mail_part">
-                                            <input type="email" name="email" id="newsletter-form-email" placeholder="<?= lang('app.email_address')?>"
-                                            class="placeholder hide-on-focus" onfocus="this.placeholder = ''"
-                                            onblur="this.placeholder = ' Email Address '">
-                                            <div class="form-icon">
-                                                <button type="submit" name="submit" id="newsletter-submit"
-                                                class="email_icon newsletter-submit button-contactForm"><img src="/assets/app/theme-charityworks/img/gallery/form.png" alt=""></button>
-                                            </div>
-                                            <div class="mt-10 info"></div>
-                                        </form>
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- footer-bottom -->
-            <div class="footer-bottom-area">
-                <div class="container">
-                    <div class="footer-border">
-                        <div class="row d-flex justify-content-between align-items-center">
-                            <div class="col-xl-8 col-lg-8 ">
-                                <div class="footer-copy-right">
-                                    <p>
-                                    <?= lang('app.copyright')?> &copy;<script>document.write(new Date().getFullYear());</script>. <?= lang('app.developedby')?> <a href="<?= prep_url('https://demasjid.com') ?>" target="_blank">Demasjid Team</a>
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-lg-3">
-                                <div class="footer-social f-right">
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="#"><i class="fab fa-instagram"></i></a>
-                                    <a href="#"><i class="fab fa-youtube"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <!-- footer start -->
+    <?= $this->include('_footer') ?>
+    <!-- footer end -->
 
     <!-- Scroll Up -->
     <div id="back-top" >
