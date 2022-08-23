36d3c60c0be51090ec33889a1a40d72a2680db51
Kamak <37825221+kamakamanulloh@users.noreply.github.com>
2022-08-23 13:55:36 +0700

    Update 2022-06-17-071435_CreatePaymentMethod.php

--- app/Database/Migrations/2022-06-17-071435_CreatePaymentMethod.php
+++ app/Database/Migrations/2022-06-17-071435_CreatePaymentMethod.php
@@ -8,11 +8,6 @@ class CreatePaymentMethod extends Migration
 {
     public function up()
     {
-
-        $this->db->disableForeignKeyChecks();
-
-
-
         $this->forge->addField([
             'id' => [
                 'type'           => 'int',
@@ -20,39 +15,22 @@ class CreatePaymentMethod extends Migration
                 'unsigned'       => true,
                 'auto_increment' => true,
             ],            
-
-            'id_bank' => [
-
             'master_payment_id' => [
-
                 'type'           => 'int',
                 'constraint'     => 11,
                 'unsigned'       => true,
             ],
-
-            'no_rek' => [
-                'type' => 'int',
-                'constraint' => 20,
-                'null' => true
-            ],
-            'nama_rek' => [
-
             'rek_no' => [
                 'type' => 'varchar',
                 'constraint' => 30,
                 'null' => true
             ],
             'rek_name' => [
-
                 'type' => 'varchar',
                 'constraint' => 50,
                 'null' => true
             ],
-
-            'id_payment_category' => [
-
             'payment_category_id' => [
-
                 'type'           => 'int',
                 'constraint'     => 11,
                 'unsigned'       => true,
@@ -73,18 +51,9 @@ class CreatePaymentMethod extends Migration
             ],
         ]);
 
-
-        $this->forge->addPrimaryKey('id');
-        $this->forge->addForeignKey('id_bank', 'bank', 'id');    
-        $this->forge->addForeignKey('id_payment_category', 'payment_category', 'id');    
-        $this->forge->createTable('payment_method', true);
-
-        $this->db->disableForeignKeyChecks();
-
         $this->forge->addPrimaryKey('id');  
         $this->forge->addForeignKey('payment_category_id', 'payment_category', 'id');    
         $this->forge->createTable('payment_method', true);
-
     }
 
     public function down()
