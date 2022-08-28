48ff5346059dd79c448276037900bbf85765139e
Moh. Zainul Muttaqin | Zalepik <51511976+mzainulmuttaqin@users.noreply.github.com>
2022-08-29 06:27:53 +0700

    Perbaikan Popup Feedback 2

--- app/Modules/BaitulMal/Views/_submodules/fundraising.php
+++ app/Modules/BaitulMal/Views/_submodules/fundraising.php
@@ -15,23 +15,9 @@
     </x-page-head>
 
     <!--x-admin-box-->
-    <div style="padding: 0 15px;" class="de_menu">
+    <div style="padding: 0 15px;">
        
         <div class="row">
-            <div class="col-md-3">
-                <div class="small-box bg-info">
-                    <div class="inner">
-                    <h3>&nbsp;</h3>
-                    <p>Tipe Donasi</p>
-                    </div>
-                    <div class="icon">
-                    <i class="icon-icon_zakat"></i>
-                    </div>
-                    <a href="http://localhost:8080/admin/baitulmal/donaturcategory" class="small-box-footer">
-                        Lihat detil <i class="fas fa-arrow-circle-right"></i>
-                    </a>
-                </div>
-            </div>
             <div class="col-md-3">
                 <div class="small-box bg-info">
                     <div class="inner">
@@ -39,7 +25,7 @@
                     <p><?= lang('crud.donaturcategory') ?></p>
                     </div>
                     <div class="icon">
-                    <i class="icon-icon_donatur"></i>
+                    <i class="fas fa-users"></i>
                     </div>
                     <a href="<?= site_url('/admin/baitulmal/donaturcategory')?>" class="small-box-footer">
                         <?= lang('app.more_info')?> <i class="fas fa-arrow-circle-right"></i>
@@ -47,16 +33,16 @@
                 </div>
             </div>
             <div class="col-md-3">
-                <div class="small-box bg-info">
+                <div class="small-box bg-warning">
                     <div class="inner">
                     <h3>&nbsp;</h3>
-                    <p>Overview</p>
+                    <p> <?= lang('crud.tim_fundraising') ?></p>
                     </div>
                     <div class="icon">
-                    <i class="icon-icon_zakat"></i>
+                    <i class="fas fa-users"></i>
                     </div>
-                    <a href="http://localhost:8080/admin/baitulmal/donaturcategory" class="small-box-footer">
-                        Lihat detil <i class="fas fa-arrow-circle-right"></i>
+                    <a href="<?= site_url('/admin/baitulmal/timfundraising')?>" class="small-box-footer">
+                        <?= lang('app.more_info')?> <i class="fas fa-arrow-circle-right"></i>
                     </a>
                 </div>
             </div>
@@ -75,29 +61,15 @@
                 </div>
             </div> -->
             <div class="col-md-3">
-                <div class="small-box">
+                <div class="small-box bg-danger">
                     <div class="inner">
                     <h3>&nbsp;</h3>
                     <p>Target Fundraising</p>
                     </div>
                     <div class="icon">
-                    <i class="icon-icon_mapping"></i>
+                    <i class="fas fa-file"></i>
                     </div>
                     <a href="<?= site_url('/admin/baitulmal/targetfundraising')?>" class="small-box-footer">
-                        <?= lang('app.more_info')?> <i class="fas fa-arrow-circle-right"></i>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="small-box">
-                    <div class="inner">
-                    <h3>&nbsp;</h3>
-                    <p> <?= lang('crud.tim_fundraising') ?></p>
-                    </div>
-                    <div class="icon">
-                    <i class="icon-icon_organisasi"></i>
-                    </div>
-                    <a href="<?= site_url('/admin/baitulmal/timfundraising')?>" class="small-box-footer">
                         <?= lang('app.more_info')?> <i class="fas fa-arrow-circle-right"></i>
                     </a>
                 </div>
--- app/Modules/BaitulMal/Views/donaturcategory/_row_info.php
+++ app/Modules/BaitulMal/Views/donaturcategory/_row_info.php
@@ -6,7 +6,7 @@
     <div class="dropdown">
         <button class="btn btn-default btn-sm dropdown-toggle btn-3-dots" type="button"  data-toggle="dropdown" aria-expanded="false"></button>
         <ul class="dropdown-menu">
-            <li><a href="<?php echo $editUrl ?>" class="dropdown-item"><i class="icon-icon_edit"></i><?php echo lang('Bonfire.edit') ?></a></li>
+            <li><a href="<?php echo $editUrl ?>" class="dropdown-item"><?php echo lang('Bonfire.edit') ?></a></li>
             <li><hr class="dropdown-divider"></li>
             <li>
                 <button class="btn" hx-delete="<?php echo $deleteUrl ?>" hx-select="#htmx-alert" hx-swap="innerHTML" hx-indicator="#htmx-request-indicator">
