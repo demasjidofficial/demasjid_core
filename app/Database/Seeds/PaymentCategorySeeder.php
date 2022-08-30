fcbe18005de758945121c86d3e4673b52f2ca764
Ahmad Afandi <ahmad.afandi85@gmail.com>
2022-08-30 14:19:09 +0700

    fix: perbaikan banyak migration yang dobel

--- app/Database/Seeds/PaymentCategorySeeder.php
+++ app/Database/Seeds/PaymentCategorySeeder.php
@@ -4,7 +4,7 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class WilayahSeeder extends Seeder
+class PaymentCategorySeeder extends Seeder
 {
     public function run()
     {
