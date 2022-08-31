81ea5c8ad6d46ed6365b206a409b0fda44cc28fa
Muhammad-Ridho <ramadhanjenius1@gmail.com>
2022-08-31 14:27:21 +0700

    memperbaiki menu pendaftaran

--- app/Modules/Pesantren/Controllers/PendaftaranController.php
+++ app/Modules/Pesantren/Controllers/PendaftaranController.php
@@ -14,27 +14,33 @@ class PendaftaranController extends AdminCrudController
     protected $baseRoute = 'admin/pesantren/pendaftaran';
     protected $langModel = 'pendaftaran';
     protected $modelName = 'App\Modules\Api\Models\PendaftaranModel';
-    public function index(){
+    public function index()
+    {
         return parent::index();
     }
 
-    public function edit($id = null){
+    public function edit($id = null)
+    {
         return parent::edit($id);
     }
 
-    public function update($id = null){
+    public function update($id = null)
+    {
         return parent::update($id);
     }
 
-    public function show($id = null){
+    public function show($id = null)
+    {
         return parent::show($id);
     }
 
-    public function create(){
+    public function create()
+    {
         return parent::create();
     }
 
-    public function delete($id = null){
+    public function delete($id = null)
+    {
         return parent::delete($id);
     }
 
@@ -43,7 +49,7 @@ class PendaftaranController extends AdminCrudController
         $model = model(PendaftaranFilter::class);
         return [
             'headers' => [
-                                    'class_id' => lang('crud.class_id'),
+                'class_id' => lang('crud.class_id'),
                 'state' => lang('crud.state'),
                 'name' => lang('crud.name'),
                 'nis' => lang('crud.nis'),
@@ -71,7 +77,7 @@ class PendaftaranController extends AdminCrudController
             ],
             'controller' => $this->getBaseController(),
             'viewPrefix' => $this->getViewPrefix(),
-			'baseRoute' => $this->getBaseRoute(),
+            'baseRoute' => $this->getBaseRoute(),
             'showSelectAll' => true,
             'data' => $model->paginate(setting('App.perPage')),
             'pager' => $model->pager
@@ -83,14 +89,14 @@ class PendaftaranController extends AdminCrudController
         $dataEdit = parent::getDataEdit($id);
         $model = new PendaftaranModel();
 
-        if(!empty($id)){
+        if (!empty($id)) {
             $data = $model->find($id);
             if (null === $data) {
                 return redirect()->back()->with('error', lang('Bonfire.resourceNotFound', [$this->langModel]));
             }
             $dataEdit['data'] = $data;
         }
-            $dataEdit['kelasItems'] = Arr::pluck(model('App\Modules\Api\Models\KelasModel')->select(['id as key','name as text'])->asArray()->findAll(), 'text', 'key');
+        $dataEdit['kelasItems'] = Arr::pluck(model('App\Modules\Api\Models\KelasModel')->select(['kelas.id as key', 'kelas.name as text'])->asArray()->findAll(), 'text', 'key');
         return $dataEdit;
     }
 }
