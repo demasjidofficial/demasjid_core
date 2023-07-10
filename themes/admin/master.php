ad0fe069a165a304d09bec343c0fdca8294a1c4b
ahmad afandi <ahmad.afandi85@gmail.com>
2023-07-10 07:18:41 +0700

    fix path logo di admin

--- themes/admin/master.php
+++ themes/admin/master.php
@@ -5,7 +5,7 @@
 
 	<title><?= config('App')->siteName ?? 'Demasjid' ?> Panel | [menu]</title>
 
-    <link rel='icon' href='assets/auth/img/demasjid-logo-icon.png' />
+    <link rel='icon' href='<?= assetUrl('auth/img/demasjid-logo-icon.png') ?>' />
 
     <!--link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous"-->
     
